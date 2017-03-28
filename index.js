d2933ad45bb6c7f6fe48766cdddea6cf062e55dd
flashbax <jkiwtco@gmail.com>
2017-03-28 11:49:14 -0400

    Commit 3

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 window.onload = function () { 
 "use strict";
 
-//create div element to go around images
+//create div element with ID container
 var div = document.createElement('div');
 div.style.width = '500px';
 div.style.height = '313px';
 div.style.border = '1px solid black';
-div.style.overflow = 'hidden';  
+//div.style.overflow = 'hidden';  
 div.id = 'imgBox'; 
 
 //make imgBox div a child of container
@@ -15,11 +15,24 @@ document.getElementById("container").appendChild(div);
 //create array with number of images
 var imgs = [1, 2, 3, 4, 5];
 
+//create div element with ID imgContainer to go around images
+var imgContainer = document.createElement('div');
+imgContainer.id = 'imgContainer';
+//make imgContainer a child element of imgBox
+document.getElementById("imgBox").appendChild(imgContainer);
+
 var i;
 for (i=1; i<=imgs.length; i++) { 
-  var path = "<img src=" + "img/" + i + ".jpg>";
-  document.getElementById("imgBox").innerHTML = path;
-  console.log(path);
+  //create image element and store in imgPath variable
+  var imgPath = "<img src=" + "img/" + i + ".jpg />";
+  //call div ID imgContainer and store in picBox variable
+  var picBox = document.getElementById("imgContainer");
+  //add image element stored in imgPath as child element of div with ID imgBox
+  picBox.innerHTML += imgPath;
+  
 }
+
+
+
 
 }; //window.onload end
